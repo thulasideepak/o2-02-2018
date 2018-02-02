84ffef4e3fabf8962e052259de0af925e4ba5599
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-02-02 15:58:33 +0530

    Create MULTIPLE.C
    multiple number of 5

---
+++ MULTIPLE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+        int i,a;
+        printf("\n Please Give The Value:");
+        scanf("%d",&a);
+        for(i=1;i<=a;i++)
+        {
+            printf("\t %d",5*i);
+        }
+        return 0;
+}
