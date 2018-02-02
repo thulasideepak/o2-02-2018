c2f6a1166fa19d8bfa584e80ad3fadafa5260285
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-02-02 16:45:27 +0530

    Update SORTED ARRAY.C

--- SORTED ARRAY.C
+++ SORTED ARRAY.C
@@ -1,23 +1,23 @@
 #include<stdio.h>
 int main()
 {
-int i,j,a[100],n,temp;
+int i,j,b[100],n,temp;
 printf("enter no of elements:");
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
-scanf("%d",&a[i]);
+scanf("%d",&b[i]);
 printf("\n array elements are %d",a[i]);
 }
   for(i=0;i<n;i++)
 {
 for(j=i+1;j<n;j++)
 {
-  if(a[i]>a[j])
+  if(b[i]>b[j])
 {
-temp=a[i];
-a[i]=a[j];
-a[j]=temp;}}}
+temp=b[i];
+b[i]=b[j];
+b[j]=temp;}}}
  printf("the sorted array is: %d",temp);
 return 0;
 }
